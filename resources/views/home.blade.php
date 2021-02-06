79efeddd379a2c8f2dc1b9ea81cd69a79d1a131b
masakikanai <m.kanai.51@gmail.com>
2021-02-06 17:32:51 +0900

    ログイン後の挙動修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <a href="/">You are logged in!</a>
                 </div>
             </div>
         </div>
